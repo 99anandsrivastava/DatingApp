15cbc1b96cc8fb60bae90f4cf9c10c39b0af95f1
ml449e <ml449e@A6159810>
2020-06-06 11:35:56 +0530

    Member detail page created

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -1,9 +1,12 @@
 import {Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './members/member-list/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver copy';
 
 
 export const appRoutes: Routes = [
@@ -16,8 +19,12 @@ export const appRoutes: Routes = [
         canActivate: [AuthGuard],
         children: [
             {
-                path: 'members' , component: MemberListComponent , canActivate: [AuthGuard]
+                path: 'members' , component: MemberListComponent , canActivate: [AuthGuard] , 
+                resolve : {users: MemberListResolver}
             },
+            {
+                path: 'members/:id' , component: MemberDetailComponent , canActivate: [AuthGuard]
+ , resolve : {user: MemberDetailResolver}            },
             {
                 path: 'messages' , component: MessagesComponent
             },
